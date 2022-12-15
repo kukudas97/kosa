6b2bee7453042f673da814a12efc420acc2c12bb
kukudas <zlawoghd@gmail.com>
2022-12-15 19:05:59 +0900

    221215

---
+++ Interceptor/src/main/java/handle/HomeController.java
@@ -0,0 +1,12 @@
+package handle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/index.htm")
+	public String index() {
+		return "index";
+	}
+}
